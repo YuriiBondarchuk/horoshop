40a3aa0d91ef27f5db4f060008acd72fe37c4c6b
Bondarchuk <yurii.bondarchuk@playvory.com>
2026-06-08 16:59:38 +0300

    - update UserServiceInterface;

--- src/Service/Contract/UserServiceInterface.php
+++ src/Service/Contract/UserServiceInterface.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 namespace App\Service\Contract;
 
+use App\Dto\CreateUserDto;
+use App\Dto\UpdateUserDto;
 use App\Entity\User;
 
 interface UserServiceInterface
 {
-//    public function create(User $user): User;
-//
-//    public function update(User $user): User;
-//
-//    public function delete(User $user): void;
-//
-//    public function find(int $id): ?User;
-//
-//    public function findAll(): array;
+    public function delete(User $user): void;
+
+    public function find(int $id): ?User;
+
+    public function findByLogin(string $login): ?User;
+
+    public function createFromDto(CreateUserDto $dto): User;
+
+    public function updateFromDto(UpdateUserDto $dto): ?User;
 }
